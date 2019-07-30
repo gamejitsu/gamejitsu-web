a9627bca9ac29d051e7b04cc1978a5edf1992941
Eugenio Depalo <eugeniodepalo@gmail.com>
2019-07-30 23:56:58 +0100

    Fix document type

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,34 @@
 import React from 'react'
-import { DocumentComponentType } from 'next/document'
-import { Head, Main, NextScript } from 'next/document'
+import NextDocument from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
+import { DocumentType } from 'next-server/dist/lib/utils'
 
-const Document: DocumentComponentType = ({ styleElement }) => (
-  <html lang="en">
-    <Head>{styleElement}</Head>
-    <body>
-      <Main />
-      <NextScript />
-    </body>
-  </html>
-)
+const Document: DocumentType = ({ children, ...props }) => <NextDocument {...props}>{children}</NextDocument>
 
-Document.getInitialProps = ({ renderPage }) => {
-  const styleSheet = new ServerStyleSheet()
-  const page = renderPage(App => props => styleSheet.collectStyles(<App {...props} />))
-  return { ...page, styleElement: styleSheet.getStyleElement() }
+Document.getInitialProps = async (ctx) => {
+  const sheet = new ServerStyleSheet()
+  const originalRenderPage = ctx.renderPage
+
+  try {
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+      })
+
+    const initialProps = await NextDocument.getInitialProps(ctx)
+
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          {sheet.getStyleElement()}
+        </>
+      )
+    }
+  } finally {
+    sheet.seal()
+  }
 }
 
 export default Document
