1dfdeddf433f91ed9715d55d987e4b6beb575632
Eugenio Depalo <eugeniodepalo@gmail.com>
2019-09-17 12:18:33 +0100

    Create deployment on GitHub

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 import { NextPage } from 'next'
 import { Layout } from '~/components'
 
-const Index: NextPage = () => <Layout></Layout>
+const Index: NextPage = () => <Layout>{process.env.API_ENDPOINT}</Layout>
 
 export default Index
