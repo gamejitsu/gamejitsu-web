8ec3e9a8791589bce1cdc9c785ceda92ec98d095
Riccardo Persiani <r.persiani92@gmail.com>
2019-10-26 14:22:42 +0100

    chore(auth): add auth

---
+++ lib/pages/auth/components/Page.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import axios from 'axios'
+import cookie from 'js-cookie'
+
+const Auth = ({ authToken }) => {
+  React.useEffect(() => {
+    cookie.set('authToken', authToken)
+    window.location = '/dashboard'
+  }, [authToken])
+  return null
+}
+
+Auth.getInitialProps = async ({ query }) => {
+  console.log('auth get props: ', query)
+  const response = await axios.post(
+    process.env.API_ENDPOINT + '/sessions',
+    {
+      data: {
+        type: 'session',
+        attributes: {
+          openid_params: new URLSearchParams(query).toString()
+        }
+      }
+    },
+    { headers: { 'Content-Type': 'application/vnd.api+json', Accept: 'application/vnd.api+json' } }
+  )
+  const authToken = response.data.data.attributes['access-token']
+  return { authToken }
+}
+
+export default Auth
