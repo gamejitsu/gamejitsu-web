c581bdb2aefff8dda4f4223436dceed4e3dcd33d
Riccardo Persiani <r.persiani92@gmail.com>
2019-11-14 19:14:07 +0000

    feat(comment-bar): move video bar function

--- lib/pages/review/components/Page.jsx
+++ lib/pages/review/components/Page.jsx
@@ -73,29 +73,29 @@ function updateVideoTimestamp(timestamp) {
   this.setState({
     videoTimestamp: timestamp
   })
+  this.videoRef.current.currentTime = timestamp
 }
 
 class Review extends React.Component {
   constructor() {
     super()
+    this.videoRef = React.createRef()
     this.state = {
       videoTimestamp: 0,
-      videoDuration: 0
+      videoDuration: 0,
     }
   }
 
   render() {
     const comments = this.props.reviewRequested.review.data.attributes.comments
-    const shownComments = comments.filter(
-      comment => {
-        const timeRange = 5
-        const videoTimestamp = this.state.videoTimestamp
-        const commentTimestamp = comment.timestamp
-        const beginTimestamp = commentTimestamp - timeRange
-        const endTimestamp = commentTimestamp + timeRange
-        return videoTimestamp > beginTimestamp && videoTimestamp < endTimestamp
-      }
-    )
+    const shownComments = comments.filter(comment => {
+      const timeRange = 5
+      const videoTimestamp = this.state.videoTimestamp
+      const commentTimestamp = comment.timestamp
+      const beginTimestamp = commentTimestamp - timeRange
+      const endTimestamp = commentTimestamp + timeRange
+      return videoTimestamp > beginTimestamp && videoTimestamp < endTimestamp
+    })
 
     return (
       <Layout title="Review">
@@ -106,7 +106,13 @@ class Review extends React.Component {
               <Text p={2}>Review Id: {this.props.reviewRequested.id}</Text>
               <Flex>
                 <Box p={0} mx="auto">
-                  <video onDurationChange={setVideoDuration.bind(this)} onTimeUpdate={setVideoTimestamp.bind(this)} width="800" controls>
+                  <video
+                    ref={this.videoRef}
+                    onDurationChange={setVideoDuration.bind(this)}
+                    onTimeUpdate={setVideoTimestamp.bind(this)}
+                    width="800"
+                    controls
+                  >
                     <source src="/static/video/sample.mp4" type="video/mp4" />
                   </video>
                 </Box>
@@ -119,7 +125,11 @@ class Review extends React.Component {
             </Box>
           </Flex>
         </Card>
-        <CommentBar comments={comments} videoDuration={this.state.videoDuration} onMoveVideoCursor={updateVideoTimestamp.bind(this)}/>
+        <CommentBar
+          comments={comments}
+          videoDuration={this.state.videoDuration}
+          onMoveVideoCursor={updateVideoTimestamp.bind(this)}
+        />
       </Layout>
     )
   }
