c9d7006b16d706110e477f66bf58fb4c3dd2098f
Riccardo Persiani <r.persiani92@gmail.com>
2019-12-21 11:51:17 +0100

    chore(coach-reviews): add coach reviews page

---
+++ pages/coach-reviews.js
@@ -0,0 +1,3 @@
+import { Page } from '~/pages/coach/coach-review-index/components'
+
+export default Page
---
+++ pages/coach-reviews/[id].js
@@ -0,0 +1,3 @@
+import { Page } from '~/pages/coach/coach-review/components'
+
+export default Page
