8bd0b0990cbfb117b4f8d03abfdc52ad6593e717
Riccardo Persiani <r.persiani92@gmail.com>
2020-01-08 21:12:10 +0000

    fix(coach-reviews): move to ts

--- pages/coach-reviews.js
+++
@@ -1,3 +0,0 @@
-import { Page } from '~/pages/coach/coach-review-index/components'
-
-export default Page
---
+++ pages/coach-reviews.ts
@@ -0,0 +1,3 @@
+import { Page } from 'gamejitsu/pages/coach/coach-review-index/components'
+
+export default Page
