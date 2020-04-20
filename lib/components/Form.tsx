0eb1e456771899745064e09c81a633187de9ba6d
Riccardo Persiani <r.persiani92@gmail.com>
2020-04-20 08:47:52 +0200

    feat: add formik to the form and fix slider

--- lib/components/Form.tsx
+++ lib/components/Form.tsx
@@ -1,19 +1,40 @@
-import styled from "styled-components"
-
 import { Box } from "rebass"
-import { FunctionComponent } from "react"
+import { Formik,Form as FormikForm, FormikProps } from "formik"
+import { ObjectSchema } from "yup"
 import { Title } from "."
 
-interface Props {
+interface Props<T> {
+  initialValues: T
+  schema: ObjectSchema
   title: string
+  onSubmit: (values: T) => Promise<void>
+  children: (handlers: FormikProps<T>) => React.ReactNode
 }
 
-const Form: FunctionComponent<Props> = ({ children, title }) =>
+type FormComponent = <T>(props: Props<T>) =>
+  React.ReactElement
+
+const Form: FormComponent = ({ children, initialValues, title, schema, onSubmit }) =>
   <Box px={4} py={2}>
     <Box mb="40px">
       <Title text={title} />
     </Box>
-    {children}
+    <Formik validationSchema={schema}
+      initialValues={initialValues}
+      onSubmit={async (
+        values,
+        { setSubmitting }
+      ) => {
+        setSubmitting(true)
+        await onSubmit(values)
+        setSubmitting(false)
+      }}>
+      {( formikHelpers ) => (
+        <FormikForm noValidate onSubmit={formikHelpers.handleSubmit}>
+          {children(formikHelpers)}
+        </FormikForm>
+      )}
+    </Formik>
   </Box>
 
 export default Form
--- lib/pages/coach/coach-signup/components/CoachSignUpForm.tsx
+++ lib/pages/coach/coach-signup/components/CoachSignUpForm.tsx
@@ -1,21 +1,51 @@
 import React, { FunctionComponent, useContext } from "react"
-import Router from "next/router"
+import titleize from 'titleize'
+import humanize from 'humanize-string'
 
 import { Box, Flex } from "rebass"
 import { Button } from "gamejitsu/components"
 import { createModel } from "gamejitsu/api"
-import { Formik } from "formik"
 import { object, string } from "yup"
 import { UserContext } from "gamejitsu/contexts"
 import { Form, FormGroup, InputGroup } from "gamejitsu/components"
-import { Slider, Divider, Card, Elevation } from "@blueprintjs/core"
-import { SkillLevel } from "gamejitsu/schemas/skillLevel"
-import titleize from 'titleize'
-import humanize from 'humanize-string'
+import { Slider, Divider, Card, Elevation, Classes } from "@blueprintjs/core"
+import { SkillLevel as SkillLevelSchema } from "gamejitsu/schemas/skillLevel"
+import { SkillLevel } from "gamejitsu/models"
+import { INTENT_SUCCESS } from "@blueprintjs/core/lib/esm/common/classes"
 
+const initialValues = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  photoUrl: "",
+  skillLevel: "medium"
+}
 
+type Values = typeof initialValues 
 
-//TODO if coach signed up, can't access to the signup page
+const skillLevels = SkillLevelSchema.types.map((t) => t.value)
+
+const isSkillLevelValid = (value: string): value is SkillLevel => (skillLevels as string[]).includes(value)
+
+const onSubmitCoach = async (values: Values): Promise<void> => {
+  const {
+    email,
+    firstName,
+    lastName,
+    photoUrl,
+    skillLevel
+  } = values
+  if (!isSkillLevelValid(skillLevel)) {
+    throw new Error(`Invalid skill level value in coach signup: ${skillLevel}`)
+  }
+  await createModel("coach", {
+    email,
+    firstName,
+    lastName,
+    photoUrl,
+    skillLevel
+  })
+}
 
 const getUser = () => {
   const user = useContext(UserContext)
@@ -23,14 +53,11 @@ const getUser = () => {
   else throw new Error("user null")
 }
 
-const skillLevels = SkillLevel.types.map((t) => t.value)
-
 const schema = object({
   firstName: string().required(),
   lastName: string().required(),
-  email: string()
-    .email()
-    .required(),
+  email: string().email().required(),
+  photoUrl: string(),
   skillLevel: string().required()
 })
 
@@ -39,50 +66,57 @@ const CoachSignUpForm: FunctionComponent = () => {
 
   let sliderVal = 0
 
-  const renderLabel = (val: number) =>
-    titleize(humanize(skillLevels[val]))
+  const renderLabel = (val: number) => {
 
-  return (
-    <Box width="700px" mx="auto" p={3}>
-      <Card elevation={Elevation.THREE}>
-        <Form title="Sign up as a coach">
-          <FormGroup label="First Name" labelFor="text-input">
-            <InputGroup id="text-input" />
-          </FormGroup>
-          <FormGroup label="Last Name" labelFor="text-input">
-            <InputGroup id="text-input" />
-          </FormGroup>
-          <FormGroup label="Email" labelFor="text-input">
-            <InputGroup id="text-input" />
-          </FormGroup>
-          <FormGroup label="Photo URL" labelFor="text-input" labelInfo="(optional)">
-            <InputGroup id="text-input" placeholder="Placeholder text" />
-          </FormGroup>
-          <FormGroup label="Skill Level" labelFor="text-input">
-            <Box width="250px">
-              <Slider
-                min={0}
-                max={3}
-                stepSize={1}
-                labelStepSize={1}
-                onChange={() => { }}
-                labelRenderer={renderLabel}
-                showTrackFill={true}
-                value={sliderVal}
-                vertical={false}
-              />
+   return titleize(humanize(skillLevels[val]))
+  }
+
+    return (
+      <Box width="700px" mx="auto" p={3}>
+        <Card elevation={Elevation.THREE}>
+          <Form title="Sign up as a coach" initialValues={initialValues} schema={schema} onSubmit={onSubmitCoach}>
+          {({ setFieldValue, values }) => (
+            <div>
+            <FormGroup label="First Name" labelFor="text-input">
+              <InputGroup id="text-input" />
+            </FormGroup>
+            <FormGroup label="Last Name" labelFor="text-input">
+              <InputGroup id="text-input" />
+            </FormGroup>
+            <FormGroup label="Email" labelFor="text-input">
+              <InputGroup id="text-input" />
+            </FormGroup>
+            <FormGroup label="Photo URL" labelFor="text-input" labelInfo="(optional)">
+              <InputGroup id="text-input" placeholder="Placeholder text" />
+            </FormGroup>
+            <FormGroup label="Skill Level" labelFor="text-input">
+              <Box width="250px">
+                <Slider
+                  min={0}
+                  max={3}
+                  stepSize={1}
+                  labelStepSize={1}
+                  onChange={(value: number) => setFieldValue("skillLevel", skillLevels[value])}
+                  labelRenderer={renderLabel}
+                  showTrackFill={true}
+                  value={skillLevels.indexOf(values.skillLevel as SkillLevel)}
+                  vertical={false}
+                  intent="success"
+                />
+              </Box>
+            </FormGroup>
+            <Box my={4}>
+              <Divider />
             </Box>
-          </FormGroup>
-          <Box my={4}>
-            <Divider />
-          </Box>
-          <Flex justifyContent="flex-end">
-            <Button text="Register" />
-          </Flex>
-        </Form>
-      </Card>
-    </Box>
-  )
-}
+            <Flex justifyContent="flex-end">
+              <Button text="Register" />
+            </Flex>
+            </div>
+             )}
+          </Form>
+        </Card>
+      </Box>
+    )
+  }
 
-export default CoachSignUpForm
+  export default CoachSignUpForm
