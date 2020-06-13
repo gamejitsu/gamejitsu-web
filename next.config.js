880dd5e53a4ca651f48ce40a739bdff9217b2c2c
Eugenio Depalo <eugeniodepalo@gmail.com>
2020-06-13 21:38:17 +0100

    io -> gg

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const withSASS = require('@zeit/next-sass')
 const webpackMerge = require('webpack-merge')
 
 const defaultEnv = {
-  API_ENDPOINT: "https://staging-api.gamejitsu.io",
-  SOCKET_ENDPOINT: "wss://staging-api.gamejitsu.io",
+  API_ENDPOINT: "https://staging-api.gamejitsu.gg",
+  SOCKET_ENDPOINT: "wss://staging-api.gamejitsu.gg",
   STRIPE_PUBLIC_KEY: "pk_test_gO4hZHVOjk7E3GjH0etoiBAO00c0qpfX0m"
 }
 
