6ca2951e4605c41c04200117d1a29420e3d63a78
Riccardo Persiani <r.persiani92@gmail.com>
2020-10-07 22:59:10 +0200

    fix: restore coach review page

---
+++ lib/pages/coach/coach-review/components/Page.tsx
@@ -0,0 +1,190 @@
+import { Flex, Box } from "rebass"
+import { NextPageContext, NextPage } from "next"
+import { Position, Toaster, Intent } from "@blueprintjs/core"
+import React, { SyntheticEvent, useRef, useState, useEffect } from "react"
+import styled from "styled-components"
+
+import { Comment } from "gamejitsu/api/types/comment"
+import { CommentBar, CommentList, CommentFormNew } from "."
+import { findModel, updateModel } from "gamejitsu/api"
+import { LayoutWithMenu } from "gamejitsu/components"
+import ReviewResource, { Review } from "gamejitsu/api/resources/review"
+import { useWarnIfUnsavedChanges } from "./RefreshPageWarner"
+import { DecoratedReview, decorateReview } from "gamejitsu/models/review"
+import { DecoratedReplay } from "gamejitsu/models/replay"
+
+interface Props {
+  review: Review
+  replay: DecoratedReplay
+}
+
+const getReview = async (ctx: NextPageContext, id: string) => {
+  const response = await findModel(ReviewResource, id, ctx)
+  return response
+}
+
+const VideoContainer = styled(Box)`
+  width: 100%;
+  border: 1px solid ${(props) => props.theme.secondaryColor};
+`
+
+const Title = styled.h1`
+  color: white;
+  font-size: 18px;
+  font-weight: bold;
+`
+
+const CoachReviewPage: NextPage<Props> = (props) => {
+  useWarnIfUnsavedChanges(true)
+
+  const [review, setReview] = useState(props.review)
+  const [videoDuration, setVideoDuration] = useState(0)
+  const [videoTimestamp, setVideoTimestamp] = useState(0)
+  const [selectedComment, setSelectedComment] = useState<Comment | null>(null)
+  const videoRef = useRef<HTMLVideoElement>(null)
+
+  const onSaveComment = async (savedComment: Comment) => {
+    const newComments = selectedComment
+      ? review.comments.map((comment) => (comment === selectedComment ? savedComment : comment))
+      : review.comments.concat(savedComment)
+    const updatedReview = {
+      ...review,
+      comments: newComments
+    }
+    setReview(updatedReview)
+    setSelectedComment(null)
+  }
+
+  const onDeleteComment = async () => {
+    const newComments = selectedComment
+      ? review.comments.filter((comment) => comment !== selectedComment)
+      : review.comments
+    const updatedReview = {
+      ...review,
+      comments: newComments
+    }
+    setReview(updatedReview)
+    setSelectedComment(null)
+  }
+
+  const onSetVideoDuration = (event: SyntheticEvent<HTMLVideoElement, Event>) => {
+    const duration = event.currentTarget.duration
+    setVideoDuration(Math.floor(duration))
+  }
+
+  const onSetVideoTimestamp = (event: SyntheticEvent<HTMLVideoElement, Event>) => {
+    const timestamp = event.currentTarget.currentTime
+    setVideoTimestamp(Math.floor(timestamp))
+  }
+
+  const onSelectComment = (comment: Comment | null) => {
+    setVideoTimestamp(comment !== null ? comment.timestamp : videoTimestamp)
+    setSelectedComment(comment)
+  }
+
+  const onDeselectComment = () => {
+    setSelectedComment(null)
+  }
+
+  const onSaveReview = async () => {
+    const AppToaster = Toaster.create({
+      className: "recipe-toaster",
+      position: Position.TOP
+    })
+    try {
+      const { data: serverReview } = await updateModel(ReviewResource, review)
+      AppToaster.show({
+        intent: Intent.SUCCESS,
+        icon: "tick",
+        message: "Review saved!"
+      })
+      setReview(serverReview)
+      if (selectedComment) {
+        setSelectedComment(serverReview.comments[review.comments.indexOf(selectedComment)])
+      }
+    } catch (error) {
+      AppToaster.show({
+        intent: Intent.DANGER,
+        icon: "warning-sign",
+        message: "Error saving review. \
+        Please try later or contact our support."
+      })
+    }
+  }
+
+  useEffect(() => {
+    if (videoRef.current) {
+      setVideoDuration(videoRef.current.duration)
+      videoRef.current.currentTime = videoTimestamp
+    } else {
+      setVideoDuration(0)
+    }
+  })
+
+  return (
+    <LayoutWithMenu title="Coach Review">
+      <Box>
+        <Flex justifyContent="center">
+          <Box>
+            <VideoContainer>
+              <video
+                ref={videoRef}
+                onDurationChange={onSetVideoDuration}
+                onTimeUpdate={onSetVideoTimestamp}
+                width="100%"
+                controls
+              >
+                <source
+                  src={props.replay.videoUrl ? props.replay.videoUrl : "/video/sample.mp4"}
+                  type="video/mp4"
+                />
+              </video>
+            </VideoContainer>
+            <Box>
+              <Box py={3}>
+                <Title>MATCH NAVIGATION</Title>
+              </Box>
+              <CommentBar
+                comments={review.comments}
+                videoDuration={videoDuration}
+                onVideoTimestampChange={setVideoTimestamp}
+                videoTimestamp={videoTimestamp}
+              />
+            </Box>
+            <Box py={3}>
+              <Title>INSERT COMMENT BY COACH</Title>
+              <CommentFormNew
+                comment={selectedComment}
+                onSave={onSaveComment}
+                onDelete={onDeleteComment}
+                onDeselect={onDeselectComment}
+                timestamp={videoTimestamp}
+              />
+            </Box>
+          </Box>
+          <Box>
+            <CommentList
+              comments={review.comments}
+              selectedComment={selectedComment}
+              onSelect={onSelectComment}
+              onSaveReview={onSaveReview}
+            />
+          </Box>
+        </Flex>
+      </Box>
+    </LayoutWithMenu>
+  )
+}
+
+CoachReviewPage.getInitialProps = async (ctx: NextPageContext) => {
+  const urlId = ctx.query.id
+  const response = await getReview(ctx, urlId.toString())
+  const reviewDecorated: DecoratedReview = decorateReview(response.data, response.included)
+  const replay: DecoratedReplay = reviewDecorated.replay
+  const review: Review = response.data
+  console.log("review:", review)
+  console.log("replay:", replay)
+  return { review, replay }
+}
+
+export default CoachReviewPage
---
+++ lib/pages/coach/coach-review/components/index.tsx
@@ -0,0 +1,9 @@
+import Page from "./Page"
+import CommentBar from "./CommentBar"
+import CommentFormNew from "./CommentFormNew"
+import CommentList from "./CommentList"
+//import UseWarnIfUnsavedChanges from "./RefreshPageWarner"
+
+const commentDuration = 10
+
+export { Page, CommentBar, commentDuration, CommentFormNew, CommentList }
