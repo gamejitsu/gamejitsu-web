e49e2098725327f76b01b814214def9c11a4f101
Riccardo Persiani <r.persiani92@gmail.com>
2020-12-14 23:12:40 +0100

    run prettier and snapshots

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -139,7 +139,12 @@ export default class App extends NextApp<Props> {
               }}
             />
             <noscript>
-              <img height="1" width="1" style={{display:"none"}} src="https://www.facebook.com/tr?id=2788245031504574&ev=PageView&noscript=1"/>
+              <img
+                height="1"
+                width="1"
+                style={{ display: "none" }}
+                src="https://www.facebook.com/tr?id=2788245031504574&ev=PageView&noscript=1"
+              />
             </noscript>
           </>
         )
