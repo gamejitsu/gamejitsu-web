69d213abfcc0abea387f081799873a4eab4e7ca6
Riccardo Persiani <r.persiani92@gmail.com>
2021-02-10 11:52:15 +0100

    feat(coach-review-request): add price, comment and matchID

--- lib/pages/coach/coach-dashboard/components/ReviewRequestCard.tsx
+++ lib/pages/coach/coach-dashboard/components/ReviewRequestCard.tsx
@@ -9,6 +9,9 @@ import { DecoratedReviewRequest } from "gamejitsu/models/review-request"
 import ReviewResource from "gamejitsu/api/resources/review"
 import { Classes, Dialog, Tooltip } from "@blueprintjs/core"
 import { breakpointDown } from "../../../../utils/mediaQueryDevices"
+import { prices } from "../../../../../public/prices"
+import { SkillLevel } from "gamejitsu/api/types/skill-level"
+const skillLevels = SkillLevel.types.map((t) => t.value)
 
 interface Props {
   reviewRequest: DecoratedReviewRequest
@@ -110,13 +113,18 @@ const ReviewRequestCard: FunctionComponent<Props> = ({ reviewRequest }) => {
                 })}
               </Flex>
               <Flex alignItems="center">
-                <Box mr="auto" mt={4}>
-                  Played with {currentPlayer.heroName}
-                </Box>
+                <Flex>
+                  <Box mr="auto" mt={4}>
+                    Played with {currentPlayer.heroName} <br />
+                    Match ID: {reviewRequest.replay.matchId} <br />
+                    Price: ${prices[skillLevels.indexOf(reviewRequest.skillLevel)].priceUSD}
+                  </Box>
+                </Flex>
                 <Box mt={4}>
                   <Button onClick={() => setAcceptReviewIsOpen(true)} text="ACCEPT REVIEW" />
                 </Box>
               </Flex>
+              <Box mt={4}>Comment: {reviewRequest.comment}</Box>
             </Box>
           </Flex>
         </Box>
