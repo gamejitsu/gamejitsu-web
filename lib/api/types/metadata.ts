382c2ffa487f3e33cc42c7255653033cfa3a6520
Riccardo Persiani <r.persiani92@gmail.com>
2021-02-26 19:59:29 +0100

    feat(metadata): add is_disconnected field

--- lib/api/types/metadata.ts
+++ lib/api/types/metadata.ts
@@ -4,16 +4,19 @@ import * as t from "io-ts"
 export interface Metadata {
   mmr: number
   isParty: boolean
+  isDisconnected: boolean
 }
 
 const decoder = t.type({
   mmr: t.number,
-  is_party: t.boolean
+  "is_party": t.boolean,
+  "is_disconnected": t.boolean
 })
 
 export const encoder = (value: Metadata) => ({
   mmr: value.mmr,
-  is_party: value.isParty
+  "is_party": value.isParty,
+  "is_disconnected": value.isDisconnected
 })
 
 export const Metadata = new t.Type(
@@ -23,7 +26,8 @@ export const Metadata = new t.Type(
     either.chain(decoder.validate(u, c), (decoded) =>
       t.success({
         mmr: decoded["mmr"],
-        isParty: decoded["is_party"]
+        isParty: decoded["is_party"],
+        isDisconnected: decoded["is_disconnected"]
       })
     ),
   String
