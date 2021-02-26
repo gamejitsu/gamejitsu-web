5275bf9793ebdda46e5ff9525156e88727c9c947
Riccardo Persiani <r.persiani92@gmail.com>
2021-02-27 00:14:02 +0100

    feat: add 3 more fields to request form

--- lib/pages/review-request/components/ReviewRequestForm.tsx
+++ lib/pages/review-request/components/ReviewRequestForm.tsx
@@ -2,9 +2,9 @@ import humanize from "humanize-string"
 import React, { FunctionComponent, useContext } from "react"
 import styled from "styled-components"
 import titleize from "titleize"
-import { Slider, Tooltip } from "@blueprintjs/core"
+import { Checkbox, Slider, Tooltip } from "@blueprintjs/core"
 import { Box, Flex } from "rebass"
-import { object, string } from "yup"
+import { boolean, number, object, string } from "yup"
 
 import { createModel } from "gamejitsu/api"
 import { DecoratedReplay } from "gamejitsu/models/replay"
@@ -46,7 +46,10 @@ const initialValues = {
   skillLevel: "high",
   replay: null,
   comment: "",
-  email: ""
+  email: "",
+  isParty: false,
+  mmr: 0,
+  isDisconnected: false
 }
 
 type Values = typeof initialValues
@@ -58,7 +61,8 @@ const isSkillLevelValid = (value: string): value is SkillLevel =>
 
 const schema = object({
   skillLevel: string().required(),
-  email: string()
+  email: string(),
+  mmr: number().required().positive().integer()
 })
 
 const getUser = () => {
@@ -75,7 +79,7 @@ const ReviewRequestForm: FunctionComponent<Props> = ({ replay }) => {
   const user = getUser()
 
   const onSubmitReviewRequest = async (values: Values): Promise<void> => {
-    const { skillLevel, comment, email } = values
+    const { skillLevel, comment, email, isParty, isDisconnected, mmr } = values
     if (!isSkillLevelValid(skillLevel)) {
       throw new Error(`Invalid skill level value in coach signup: ${skillLevel}`)
     }
@@ -83,7 +87,10 @@ const ReviewRequestForm: FunctionComponent<Props> = ({ replay }) => {
     if (replay === undefined) {
       throw new Error(`Invalid replay`)
     }
-    const metadata = { mmr: 1200, isParty: true }
+    const metadata = { mmr, isParty, isDisconnected }
+    console.log("isParty:", isParty)
+    console.log("isDisconnected:", isDisconnected)
+    console.log("mmr:", mmr)
     redirectToCheckout({ comment, skillLevel, replayId: replay.id, email, metadata })
   }
 
@@ -102,6 +109,11 @@ const ReviewRequestForm: FunctionComponent<Props> = ({ replay }) => {
       errors.skillLevel = "Skill level is required"
     } else if (!(skillLevels as string[]).includes(values.skillLevel)) {
       errors.skillLevel = "Invalid skill level"
+    }
+    if (!values.mmr) {
+      errors.mmr = "MMR is required"
+    } else if (isNaN(values.mmr)) {
+      errors.mmr = "Invalid MMR"
     }
     return errors
   }
@@ -154,6 +166,7 @@ const ReviewRequestForm: FunctionComponent<Props> = ({ replay }) => {
               </FormGroup>
               Insert email if you want to receive status notifications
               {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+              {formik.errors.mmr ? <div>{formik.errors.mmr}</div> : null}
               <FormGroup label="Email" labelFor="email">
                 <InputGroup
                   onChange={formik.handleChange("email")}
@@ -167,12 +180,27 @@ const ReviewRequestForm: FunctionComponent<Props> = ({ replay }) => {
                   <InputGroup
                     leftIcon="warning-sign"
                     onChange={formik.handleChange("comment")}
-                    id="text-input"
+                    id="text-input-comment"
+                  />
+                </Tooltip>
+              </FormGroup>
+              <FormGroup label="MMR" labelFor="number-input">
+                <Tooltip content="Add a number which should approximately represent your MMR, if you don't know use 0 instead">
+                  <InputGroup
+                    onChange={formik.handleChange("mmr")}
+                    name="MMR"
+                    id="MMR"
                   />
                 </Tooltip>
               </FormGroup>
               <FormGroup label="Price" labelFor="text-input">
                 ${prices[skillLevels.indexOf(formik.values.skillLevel as SkillLevel)].priceUSD}
+              </FormGroup>
+              <FormGroup label="isParty" labelFor="checkbox">
+                <Checkbox checked={formik.values.isParty} label="isParty" onChange={() => formik.setFieldValue("isParty", !formik.values.isParty)} />
+              </FormGroup>
+              <FormGroup label="isDisconnected" labelFor="checkbox">
+                <Checkbox checked={formik.values.isDisconnected} label="isDisconnected" onChange={() => formik.setFieldValue("isDisconnected", !formik.values.isDisconnected)} />
               </FormGroup>
             </div>
           )}
