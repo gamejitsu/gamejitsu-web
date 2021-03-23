bb6b59656f2f0456ba2c2863bdda7d1945bbdd9a
federico giorni <giornifederico@gmail.com>
2021-03-23 01:27:54 +0100

    Switch carousel style from scss to css

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -2,21 +2,25 @@ import { mount } from "enzyme"
 import React from "react"
 import { ThemeProvider } from "styled-components"
 import { theme } from "gamejitsu"
-import IndexPage from "../pages/index"
+import DemoPage from "../pages/demo"
 import { UserContext } from "../lib/contexts"
 
 const user = {
   type: "user" as "user",
   username: "userTest",
   isSyncingReplays: false,
-  steamId: "1"
+  steamId: "1",
+  hasPublicProfile: true,
+  email: "foobar@example.com",
+  coachId: "10328f42-849e-405d-affc-15373340w0bw",
+  id: "10328f42-849e-405d-affc-3537w340cwbf"
 }
 
 test("Index page should match snapshot", () => {
   const home = mount(
     <ThemeProvider theme={theme}>
       <UserContext.Provider value={user}>
-        <IndexPage />
+        <DemoPage />
       </UserContext.Provider>
     </ThemeProvider>
   )
